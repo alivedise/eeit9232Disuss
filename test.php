645c661496e5705545c7ef1863298f295d4f78e6
SakueiNozomi <Macintosh@SakueiNozomi.Home>
2017-04-01 22:05:12 +0800

    test connect database

--- test.php
+++ test.php
@@ -1,8 +1,25 @@
+<!DOCTYPE html>
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
+<head>
+	<title></title>
+</head>
+<body>
+
+<?php
+$servername = "ja-cdbr-azure-east-a.cloudapp.net";
+$username = "bb763638944ffa";
+$password = "5b724968";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
+
+</body>
 </html>
+
