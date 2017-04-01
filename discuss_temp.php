324e9f5e193494dafce757e195119b8201cdd519
SakueiNozomi <Macintosh@SakueiNozomi.Home>
2017-04-02 06:37:59 +0800

    test php to db23

---
+++ discuss_temp.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+
+	<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+	<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+	<title></title>
+</head>
+<body>
+	<div name="showarticlejsp" class="showarticle">
+		<table>
+			<tr>
+				<!-- 文章相片 -->
+				<td width="450px"><h5>
+						標題<small>作者：</small>
+					</h5></td>
+				<!-- 文章標題 -->
+
+			</tr>
+
+			<tr>
+				<td>內文：<a></a>(繼續閱讀)
+				</td>
+				<!-- 內文 -->
+			</tr>
+				<td colspan="2">修改文章按鈕</td>
+		</table>
+	</div>
+
+</body>
+</html>
