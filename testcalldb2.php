d7ef097844b4417c0fc06717d430471686ee5002
SakueiNozomi <Macintosh@SakueiNozomi.Home>
2017-04-01 23:27:54 +0800

    cp & pst test2

--- testcalldb2.php
+++ testcalldb2.php
@@ -5,7 +5,7 @@
 	<META HTTP-EQUIV="Cache-Control" CONTENT="no-cache" />
 	<META HTTP-EQUIV="Expires" CONTENT="0" />
 	<meta name="viewport" http-equiv="Content-Type"
-	content="text/html; charset=UTF-8" />
+	content="text/html" charset="UTF-8" />
 	<meta name="viewport"
 	content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
 	<title></title>
