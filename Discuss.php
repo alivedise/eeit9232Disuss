1717a78eca7a84a15c1ef76c445e3c5459bea775
SakueiNozomi <Macintosh@SakueiNozomi.Home>
2017-04-01 23:10:27 +0800

    add new php

---
+++ Discuss.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<META HTTP-EQUIV="Pragma" CONTENT="no-cache" />
+	<META HTTP-EQUIV="Cache-Control" CONTENT="no-cache" />
+	<META HTTP-EQUIV="Expires" CONTENT="0" />
+	<meta name="viewport" http-equiv="Content-Type"
+	content="text/html; charset=UTF-8" />
+	<meta name="viewport"
+	content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+	<title></title>
+	<!-- jquery置頂 -->
+	<script src='https://code.jquery.com/jquery-3.1.0.min.js'></script>
+
+	<!-- bootstrap css-->
+	<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+
+	<!-- 本機 -->
+	
+	<link rel='stylesheet' type='text/css' href="">
+	<script src="" type="text/javascript" charset="utf-8"></script>
+
+	<!-- jquery plugins -->
+
+	<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/jquery.parallax-1.1.3.js"></script>
+	<script type="text/javascript" src="js/jquery.localscroll-1.2.7-min.js"></script>
+	<script type="text/javascript" src="js/jquery.scrollTo-1.4.2-min.js"></script>
+	<script type="text/javascript" src="js/jquery.inview.js"></script>
+
+
+	<style type="text/css" media="screen"></style>
+</head>
+<body>
+	
+	<div id="disarticle">
+
+		
+		
+	</div>
+
+</body>
+</html>
---
+++ testcalldb.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<META HTTP-EQUIV="Pragma" CONTENT="no-cache" />
+	<META HTTP-EQUIV="Cache-Control" CONTENT="no-cache" />
+	<META HTTP-EQUIV="Expires" CONTENT="0" />
+	<meta name="viewport" http-equiv="Content-Type"
+	content="text/html; charset=UTF-8" />
+	<meta name="viewport"
+	content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+	<title></title>
+</head>
+<body>
+
+	<p>
+		<?php 
+		$servername = "ja-cdbr-azure-east-a.cloudapp.net";
+		$username = "bb763638944ffa";
+		$password = "5b724968";
+
+// Create connection
+		$conn = mysqli_connect($servername, $username, $password) or die("連不到資料庫");
+		mysqli_select_db($conn,"discuss")
+
+		$sql="SELECT * FROM discuss";
+
+		mysqli_query($conn, "SET CHARACTER SET utf8");
+		mysqli_query($conn, "SET collation_connection = 'utf8_general_ci'")
+
+		$result=mysqli_query($conn,$sql);
+		?>
+	</p>
+
+</body>
+</html>
