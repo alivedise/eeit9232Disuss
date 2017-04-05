77d82d25f274d8162b752493e7d4d9fd91ed2000
SakueiNozomi <Macintosh@SakueiNozomi.local>
2017-04-05 15:59:22 +0800

    test php dis 8

--- add.php
+++ add.php
@@ -9,7 +9,8 @@ $email=$_POST["email"];
 $context=$_POST["context"];
 $password=$_POST["password"];
 /* 將欄位資料加入資料庫 */
-$sql_insert="INSERT INTO discuss (title,name,email,context,password) VALUES ('$title','$name','$email','$context','$password')";
+$sql_insert="INSERT INTO discuss (title,name,email,context,password) 
+VALUES ('$title','$name','$email','$context','$password')";
 mysql_query($sql_insert);
 ?>
 
