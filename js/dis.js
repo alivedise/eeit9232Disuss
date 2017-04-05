2e2528ec65888dc8bff0737c1a5aad846d7253d0
SakueiNozomi <Macintosh@SakueiNozomi.local>
2017-04-05 19:18:35 +0800

    test css and some link 23

--- js/dis.js
+++ js/dis.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-	("#gotop").click(function(){
-		$("html,body").animate({scrollTop:$("#top").offset().top},800);
+	$("#gotop").click(function(){
+		$("html,body").animate({ scrollTop: 0 }, 'slow');
 
 		return false;
 	})
