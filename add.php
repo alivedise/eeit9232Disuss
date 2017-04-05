a1f21c2d24dfe588ac21a67be83c9233f5d9f1f3
SakueiNozomi <Macintosh@SakueiNozomi.local>
2017-04-05 16:45:06 +0800

    test php dis 16

--- add.php
+++ add.php
@@ -1,34 +1,41 @@
 <html>
 <head>
-<title>討論區</title>
+	<title>討論區</title>
 
 </head>
 <body>
 
-<?php
+	<?php
 
-$title=$_POST["title"];
-$name=$_POST["name"];
-$email=$_POST["email"];
-$context=$_POST["context"];
-$password=$_POST["password"];
-
-echo("$title"."$name"."$email"."$context"."$password");
-	
-
- ?>
-
-<!-- <?php
-
-$servername = "ja-cdbr-azure-east-a.cloudapp.net";
-$username = "bb763638944ffa";
-$password = "5b724968";
-$dbname = "discuss";		
+	$servername = "ja-cdbr-azure-east-a.cloudapp.net";
+	$username = "bb763638944ffa";
+	$password = "5b724968";
+	$dbname = "discuss";		
 
 		// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-?> -->
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	echo "Connected successfully";
+
+	?>
+
+	<?php
+
+	$title=$_POST["title"];
+	$name=$_POST["name"];
+	$email=$_POST["email"];
+	$context=$_POST["context"];
+	$password=$_POST["password"];
+
+	echo("$title"."$name"."$email"."$context"."$password");
+	
+
+	?>
+
+
 
 
 </body>
