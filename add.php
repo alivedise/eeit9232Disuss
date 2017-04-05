38b7a3666f357cd9e3e58511601adf078b80ea9b
SakueiNozomi <Macintosh@SakueiNozomi.local>
2017-04-05 16:36:53 +0800

    test php dis 15

--- add.php
+++ add.php
@@ -13,7 +13,7 @@ $email=$_POST["email"];
 $context=$_POST["context"];
 $password=$_POST["password"];
 
-echo("$title"."$name");
+echo("$title"."$name"."$email"."$context"."$password");
 	
 
  ?>
@@ -40,19 +40,4 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 
 
-<!-- 
-<?header("location:discuss_temp.php");?>    
-<?php
-include("db.php");  
-/* 接收表單資料 */
-$title=$_POST["title"];
-$name=$_POST["name"];
-$email=$_POST["email"];
-$context=$_POST["context"];
-$password=$_POST["password"];
-/* 將欄位資料加入資料庫 */
-$sql_insert="INSERT INTO discuss (title,name,email,context,password) 
-VALUES ('$title','$name','$email','$context','$password')";
-mysql_query($sql_insert);
-?>
- -->
+
