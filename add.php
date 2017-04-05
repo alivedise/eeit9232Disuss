af48861d70b51583d77470068a4afe50c0f014c3
SakueiNozomi <Macintosh@SakueiNozomi.local>
2017-04-05 16:33:55 +0800

    test php dis 14

--- add.php
+++ add.php
@@ -1,4 +1,46 @@
+<html>
+<head>
+<title>討論區</title>
 
+</head>
+<body>
+
+<?php
+
+$title=$_POST["title"];
+$name=$_POST["name"];
+$email=$_POST["email"];
+$context=$_POST["context"];
+$password=$_POST["password"];
+
+echo("$title"."$name");
+	
+
+ ?>
+
+<!-- <?php
+
+$servername = "ja-cdbr-azure-east-a.cloudapp.net";
+$username = "bb763638944ffa";
+$password = "5b724968";
+$dbname = "discuss";		
+
+		// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+?> -->
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+<!-- 
 <?header("location:discuss_temp.php");?>    
 <?php
 include("db.php");  
@@ -13,4 +55,4 @@ $sql_insert="INSERT INTO discuss (title,name,email,context,password)
 VALUES ('$title','$name','$email','$context','$password')";
 mysql_query($sql_insert);
 ?>
-
+ -->
