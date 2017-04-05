7783e344fa9812c1a67c6f250e4deaa1726a9ef4
SakueiNozomi <Macintosh@SakueiNozomi.local>
2017-04-05 15:30:43 +0800

    test php dis 7

--- add.php
+++ add.php
@@ -1,21 +1,7 @@
-<html>
-<head>
-<title>討論區</title>
-</head>
-<body>
 
 <?header("location:discuss_temp.php");?>    
 <?php
-
-$servername = "ja-cdbr-azure-east-a.cloudapp.net";
-$username = "bb763638944ffa";
-$password = "5b724968";
-$dbname = "discuss";		
-
-		// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-
+include("db.php");  
 /* 接收表單資料 */
 $title=$_POST["title"];
 $name=$_POST["name"];
@@ -23,10 +9,7 @@ $email=$_POST["email"];
 $context=$_POST["context"];
 $password=$_POST["password"];
 /* 將欄位資料加入資料庫 */
-$sql_insert="INSERT INTO discuss (title,name,email,context,password)
-VALUES ('$title','$name','$email','$context','$password')";
+$sql_insert="INSERT INTO discuss (title,name,email,context,password) VALUES ('$title','$name','$email','$context','$password')";
 mysql_query($sql_insert);
 ?>
 
-</body>
-</html>
---
+++ db.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>討論區</title>
+
+</head>
+<body>
+
+<?php
+
+$servername = "ja-cdbr-azure-east-a.cloudapp.net";
+$username = "bb763638944ffa";
+$password = "5b724968";
+$dbname = "discuss";		
+
+		// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+?>
+
+
+</body>
+</html>
